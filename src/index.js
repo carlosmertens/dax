b7484ccfddfc62c233c8db482cb959d8af716084
Carlos Mertens <mertenscarlos@yahoo.com>
2020-09-29 22:33:06 +0200

    add redux-persist

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+
 import App from './App';
+// import Spinner from './components/Spinner';
+
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import reduxPromise from 'redux-promise';
 
-const theStore = applyMiddleware(reduxPromise)(createStore)(rootReducer);
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2,
+  blacklist: ['siteModal'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const theStore = applyMiddleware(reduxPromise)(createStore)(persistedReducer);
+const persistor = persistStore(theStore);
 
 ReactDOM.render(
   <Provider store={theStore}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
-
-
