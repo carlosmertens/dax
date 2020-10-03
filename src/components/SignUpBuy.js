b666f78b7de5c8b7293ee66c2f40f281e1adfc94
Carlos Mertens <mertenscarlos@yahoo.com>
2020-10-03 16:10:30 +0200

    refactor Sign Up Buy component

--- src/components/SignUpBuy.js
+++ src/components/SignUpBuy.js
@@ -10,140 +10,72 @@ import LoginBuy from './LoginBuy';
 
 import { Redirect } from 'react-router-dom';
 
-const SignUpBuy = (props) => {
+const SignUp = (props) => {
   const idioma = props.idioma;
 
-  // const [NomCliente, setNomCliente] = useState('');
-  // const [NomContacto, setNomContacto] = useState('');
-  // const [NumNit, setNumNit] = useState('');
-  // const [CodPais, setCodPais] = useState('');
-  // const [CodCiudad, setCodCiudad] = useState('');
-  // const [Direccion, setDireccion] = useState('');
-  // const [NumTel1, setNumTel1] = useState('');
-  // const [NumTel2, setNumTel2] = useState('');
+  const [NomCliente, setNomCliente] = useState('');
+  const [NomContacto, setNomContacto] = useState('');
+  const [NumNit, setNumNit] = useState('');
+  const [CodPais, setCodPais] = useState('');
+  const [CodCiudad, setCodCiudad] = useState('');
+  const [Direccion, setDireccion] = useState('');
+  const [NumTel1, setNumTel1] = useState('');
+  const [NumTel2, setNumTel2] = useState('');
   const [Mail, setMail] = useState('');
-  // const [NomUsuario, setNomUsuario] = useState('');
-  // const [LogUsuario, setLogUsuario] = useState('');
+  const [NomUsuario, setNomUsuario] = useState('');
+  const [LogUsuario, setLogUsuario] = useState('');
   const [Contrasena, setContrasena] = useState('');
+  const [logged, setLogged] = useState(false);
 
-  // Funcion para cerrar el modal al enviar formulario
-  // Conponent to close the modal
   const closeModal = () => {
     props.openModal('closed', '');
   };
 
-  // ============================= START TEST WITH AIRBNB-API============================== //
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const url = 'https://airbnb-api.robertbunch.dev/users/signup';
+
+    const url = 'http://www.wp.daxparts.com/api/cliente/guardarCliente';
     const data = {
-      email: Mail,
-      password: Contrasena,
+      IdCliente: '0',
+      NomCliente: NomCliente,
+      NomContacto: NomContacto,
+      NumNit: NumNit,
+      CodPais: CodPais,
+      CodCiudad: CodCiudad,
+      Direccion: Direccion,
+      NumTel1: NumTel1,
+      NumTel2: NumTel2,
+      Mail: Mail,
+      NomUsuario: NomUsuario,
+      LogUsuario: LogUsuario,
+      Contrasena: Contrasena,
     };
+
     const resp = await axios.post(url, data);
-
-    if (resp.data.msg === 'userExists') {
+    if (resp.data.estado === 'OK') {
+      setLogged(true);
+    } else {
       swal({
-        title: 'Email Exists',
-        text: 'Email provided is already registered!',
+        title: 'REGISTRO INCORRECTO!',
+        text:
+          'Lo sentimos, no pudimos conectar con la base de datos. Intentalo de nuevo!',
         icon: 'error',
       });
-    } else if (resp.data.msg === 'invalidData') {
-      swal({
-        title: 'Invalid Email/Password',
-        text: 'Please verify your email and password!',
-        icon: 'error',
-      });
-    } else if (resp.data.msg === 'userAdded') {
-      props.regAction(resp.data);
-      closeModal();
     }
+    closeModal();
   };
-  console.log(props.auth);
-  // =============================END TEST WITH AIRBNB-API============================== //
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   closeModal();
-
-  //   console.log(NomCliente);
-  //   console.log(NomContacto);
-  //   console.log(NumNit);
-  //   console.log(CodPais);
-  //   console.log(CodCiudad);
-  //   console.log(Direccion);
-  //   console.log(NumTel1);
-  //   console.log(NumTel2);
-  //   console.log(Mail);
-  //   console.log(LogUsuario);
-  //   console.log(Contrasena);
-
-  //   ***** Llamar DaxParts API *****
-  //   ***** Call DaxParts API *****
-  //   const url = 'http://www.wp.daxparts.com/api/cliente/guardarCliente';
-  //   const data = {
-  //     IdCliente: '0',
-  //     NomCliente: NomCliente,
-  //     NomContacto: NomContacto,
-  //     NumNit: NumNit,
-  //     CodPais: CodPais,
-  //     CodCiudad: CodCiudad,
-  //     Direccion: Direccion,
-  //     NumTel1: NumTel1,
-  //     NumTel2: NumTel2,
-  //     Mail: Mail,
-  //     NomUsuario: NomUsuario,
-  //     LogUsuario: LogUsuario,
-  //     Contrasena: Contrasena,
-  //   };
-  //   ***** Enviar pedido POST a la API
-  //   ***** Call POST request *****
-  //   const response = await axios.post(url, data);
-  //   console.log(response);
-  // };
 
   return (
     <>
-      {props.auth.email ? (
+      {logged ? (
         <Redirect to='/comprar' />
       ) : (
         <>
           <div className='modal-logo d-flex justify-content-center'>
             <img src={navLogo} alt='Dax Logo' />
           </div>
-          {/* =============================TEST START============================ */}
-          <div className='modal-body'>
-            <form onSubmit={handleSubmit}>
-              <h4>{idioma.crear.titulo1}</h4>
-              <div className='form-group d-flex justify-content-center'>
-                <input
-                  type='email'
-                  className='form-control mr-sm-2'
-                  placeholder='Email'
-                  onChange={(e) => setMail(e.target.value)}
-                  value={Mail}
-                />
-              </div>
-              <div className='form-group d-flex justify-content-center'>
-                <input
-                  type='password'
-                  className='form-control mr-sm-2'
-                  placeholder={idioma.crear.password1}
-                  onChange={(e) => setContrasena(e.target.value)}
-                  value={Contrasena}
-                />
-              </div>
-              <div className='boton-form'>
-                <button type='submit' className='btn'>
-                  {idioma.crear.botonCrear}
-                </button>
-              </div>
-            </form>
-          </div>
-          {/* =============================TEST END============================ */}
 
-          {/* =========================START DAX CONTENT======================= */}
-          {/* <div className='modal-body'>
+          <div className='modal-body'>
             <form onSubmit={handleSubmit}>
               <h4>{idioma.crear.titulo1}</h4>
               <div className='form-group d-flex justify-content-center'>
@@ -153,6 +85,7 @@ const SignUpBuy = (props) => {
                   placeholder={idioma.crear.empresa}
                   onChange={(e) => setNomCliente(e.target.value)}
                   value={NomCliente}
+                  required
                 />
               </div>
 
@@ -163,6 +96,7 @@ const SignUpBuy = (props) => {
                   placeholder={idioma.crear.nombre}
                   onChange={(e) => setNomContacto(e.target.value)}
                   value={NomContacto}
+                  required
                 />
               </div>
 
@@ -173,6 +107,7 @@ const SignUpBuy = (props) => {
                   placeholder={idioma.crear.numero}
                   onChange={(e) => setNumNit(e.target.value)}
                   value={NumNit}
+                  required
                 />
               </div>
 
@@ -183,6 +118,7 @@ const SignUpBuy = (props) => {
                   placeholder={idioma.crear.pais}
                   onChange={(e) => setCodPais(e.target.value)}
                   value={CodPais}
+                  required
                 />
               </div>
 
@@ -193,6 +129,7 @@ const SignUpBuy = (props) => {
                   placeholder={idioma.crear.ciudad}
                   onChange={(e) => setCodCiudad(e.target.value)}
                   value={CodCiudad}
+                  required
                 />
               </div>
 
@@ -203,6 +140,7 @@ const SignUpBuy = (props) => {
                   placeholder='Direccion'
                   onChange={(e) => setDireccion(e.target.value)}
                   value={Direccion}
+                  required
                 />
               </div>
 
@@ -213,6 +151,7 @@ const SignUpBuy = (props) => {
                   placeholder={idioma.crear.telefono1}
                   onChange={(e) => setNumTel1(e.target.value)}
                   value={NumTel1}
+                  required
                 />
               </div>
 
@@ -223,6 +162,7 @@ const SignUpBuy = (props) => {
                   placeholder={idioma.crear.telefono2}
                   onChange={(e) => setNumTel2(e.target.value)}
                   value={NumTel2}
+                  required
                 />
               </div>
 
@@ -233,6 +173,7 @@ const SignUpBuy = (props) => {
                   placeholder='Email'
                   onChange={(e) => setMail(e.target.value)}
                   value={Mail}
+                  required
                 />
               </div>
 
@@ -245,6 +186,7 @@ const SignUpBuy = (props) => {
                   placeholder={idioma.crear.nomUsuario}
                   onChange={(e) => setNomUsuario(e.target.value)}
                   value={NomUsuario}
+                  required
                 />
               </div>
 
@@ -255,6 +197,7 @@ const SignUpBuy = (props) => {
                   placeholder={idioma.crear.usuario}
                   onChange={(e) => setLogUsuario(e.target.value)}
                   value={LogUsuario}
+                  required
                 />
               </div>
 
@@ -265,6 +208,7 @@ const SignUpBuy = (props) => {
                   placeholder={idioma.crear.password1}
                   onChange={(e) => setContrasena(e.target.value)}
                   value={Contrasena}
+                  required
                 />
               </div>
 
@@ -274,21 +218,15 @@ const SignUpBuy = (props) => {
                 </button>
               </div>
             </form>
-          </div> */}
-          {/* =========================START DAX CONTENT======================= */}
+          </div>
+
           <div className='modal-footer d-flex justify-content-center'>
             <div>
               {idioma.crear.cambiarModal}{' '}
               <span
                 className='pointer'
                 onClick={() => {
-                  props.openModal(
-                    'open',
-                    <LoginBuy
-                      idioma={idioma}
-                      intCodRepuesto={props.intCodRepuesto}
-                    />
-                  );
+                  props.openModal('open', <LoginBuy idioma={idioma} />);
                 }}
                 style={{ color: '#fca728' }}>
                 {idioma.crear.cambiarEnlace}
@@ -318,4 +256,4 @@ function mapDispatchToProps(dispacher) {
   );
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SignUpBuy);
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
