c09ffb05da8df590ec2f0959cbd0f149d0b91036
Carlos Mertens <mertenscarlos@yahoo.com>
2020-12-27 14:31:28 +0100

    clean up language texts

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -23,19 +23,6 @@ const Homepage = ({ language, onChangeLanguage }) => {
     }
   };
 
-  // <!-- Google Tag Manager -->
-  // const googleTagManager = (function (w, d, s, l, i) {
-  //   w[l] = w[l] || [];
-  //   w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-  //   var f = d.getElementsByTagName(s)[0],
-  //     j = d.createElement(s),
-  //     dl = l !== 'dataLayer' ? '&l=' + l : '';
-  //   j.async = true;
-  //   j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-  //   f.parentNode.insertBefore(j, f);
-  // })(window, document, 'script', 'dataLayer', 'GTM-WLPRK4K');
-  // <!-- End Google Tag Manager -->
-
   return (
     <React.Fragment>
       {cotizar ? (
