d6c3782999f9aea1f8f338cb76c781937006e157
Carlos Mertens <mertenscarlos@yahoo.com>
2021-03-02 23:09:14 +0100

    Add WhatsApp svg logo

--- src/components/WhatsApp.js
+++ src/components/WhatsApp.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { FaWhatsappSquare } from 'react-icons/fa';
+import { ReactComponent as WhatsAppLogo } from '../img/whatsapp-icon.svg';
+// import { FaWhatsappSquare } from 'react-icons/fa';
 
 const WhatsApp = () => {
   return (
     <div className='fixed-bottom d-flex justify-content-end'>
       <a
-        className='p-2'
+        className='p-3'
         href='https://api.whatsapp.com/send?phone=4917677200357&text=Hola!'>
-        <FaWhatsappSquare size='60' color='#25D366' />
+        {/* <FaWhatsappSquare size='60' color='#25D366' /> */}
+        <WhatsAppLogo />
       </a>
     </div>
   );
