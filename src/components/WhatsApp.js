1000d618cd0107971db1ed2579dbc2e02f8359bd
Carlos Mertens <mertenscarlos@yahoo.com>
2021-03-03 15:17:11 +0100

    Remove WhatsApp pmg functionality

--- src/components/WhatsApp.js
+++ src/components/WhatsApp.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ReactComponent as WhatsAppLogo } from '../img/whatsapp-icon.svg';
-// import { FaWhatsappSquare } from 'react-icons/fa';
 
 const WhatsApp = () => {
   return (
@@ -8,7 +7,6 @@ const WhatsApp = () => {
       <a
         className='p-3'
         href='https://api.whatsapp.com/send?phone=4917677200357&text=Hola!'>
-        {/* <FaWhatsappSquare size='60' color='#25D366' /> */}
         <WhatsAppLogo />
       </a>
     </div>
